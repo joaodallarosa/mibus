d9b1940212bf27587ca788514db3ad124154f31f
Joao Dalla Rosa <joaofilipedallarosa@gmail.com>
2016-02-21 17:33:22 -0300

    Fixing bugs on the map modal and cleaning more code

--- mibus/www/screens/list-view/list-view.js
+++ mibus/www/screens/list-view/list-view.js
@@ -3,6 +3,7 @@ angular.module('mibus').controller("ListViewController", function($scope, $state
   $scope.routes = [];
   $scope.searchText = '';
   $scope.isSearching = false;
+  $scope.hasLoadedMap = false;
 
   $scope.getRoutes = function(routeStop) {
     $scope.isSearching = true;
@@ -30,6 +31,7 @@ angular.module('mibus').controller("ListViewController", function($scope, $state
     $state.go('map-view');
   };
 
+  // Google maps modal handling street selection
   $ionicModal.fromTemplateUrl('screens/map-modal/map-modal.html', {
     scope: $scope,
     animation: 'slide-in-up',
@@ -39,73 +41,67 @@ angular.module('mibus').controller("ListViewController", function($scope, $state
   $scope.openModal = function() {
     $scope.modal.show();
 
-    if ($scope.map == undefined) {
-      $ionicLoading.show({
-        template: "Loading..."
-      });
-      var options = {
-        timeout: 10000,
-        enableHighAccuracy: true
-      };
+    // if (!$scope.hasLoadedMap) {
+    $ionicLoading.show({
+      template: "Loading Google Maps..."
+    });
+    var options = {
+      timeout: 5000
+    };
 
-      $scope.selectedStreet = '';
-      $cordovaGeolocation.getCurrentPosition(options).then(function(position) {
+    $scope.selectedStreet = '';
 
-        var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    // Florianopolis Lat/Long
+    var latLng = new google.maps.LatLng("-27.5928351", "-48.5327725");
 
-        var mapOptions = {
-          center: latLng,
-          zoom: 15,
-          mapTypeId: google.maps.MapTypeId.ROADMAP,
-          disableDefaultUI: true
-        };
+    var mapOptions = {
+      center: latLng,
+      zoom: 14,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      disableDefaultUI: true
+    };
 
-        $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    $scope.geocoder = new google.maps.Geocoder();
+    google.maps.event.addListener($scope.map, 'click', function(event) {
+      placeMarker(event.latLng);
+    });
 
-        $scope.geocoder = new google.maps.Geocoder();
-
-        google.maps.event.addListener($scope.map, 'click', function(event) {
-          placeMarker(event.latLng);
+    var marker;
+    function placeMarker(location) {
+      if (marker) {
+        marker.setPosition(location);
+      } else {
+        marker = new google.maps.Marker({
+          position: location,
+          map: $scope.map
         });
-
-        var marker;
-
-        function placeMarker(location) {
-          if (marker) {
-            marker.setPosition(location);
-          } else {
-            marker = new google.maps.Marker({
-              position: location,
-              map: $scope.map
-            });
-          }
-          getAddress(location);
-        }
-
-        function getAddress(latLng) {
-          $scope.geocoder.geocode({
-              'latLng': latLng
-            },
-            function(results, status) {
-              if (status == google.maps.GeocoderStatus.OK) {
-                if (results[0]) {
-                  $scope.selectedStreet = results[0].formatted_address.split(',')[0];
-                  $scope.searchText = results[0].formatted_address.split(',')[0];
-                  $scope.$apply();
-                  console.log('Address: ', results[0].formatted_address.split(',')[0])
-                } else {
-                  console.log('No results...');
-                }
-              } else {
-                console.log('Geocoder error...');
-              }
-            });
-        }
-        $ionicLoading.hide();
-      }, function(error) {
-        console.log("Could not get location");
-      });
+      }
+      getAddress(location);
     }
+
+    function getAddress(latLng) {
+      $scope.geocoder.geocode({
+          'latLng': latLng
+        },
+        function(results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            if (results[0]) {
+              $scope.selectedStreet = results[0].formatted_address.split(',')[0];
+              $scope.searchText = results[0].formatted_address.split(',')[0];
+              $scope.$apply();
+            } else {
+              console.log('No results...');
+            }
+          } else {
+            $ionicLoading.hide();
+            $scope.modal.hide();
+            console.log('Geocoder error...');
+          }
+        });
+    }
+    $ionicLoading.hide();
+    $scope.hasLoadedMap = true;
   };
 
   $scope.closeModal = function() {
